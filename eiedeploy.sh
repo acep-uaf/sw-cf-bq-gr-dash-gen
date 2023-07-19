8a894ffcf0504f2698d81db0245d845086643873
jody <thisjody@gmail.com>
2023-07-19 10:59:15 -0700

    first commit

---
+++ eiedeploy.sh
@@ -0,0 +1,10 @@
+gcloud functions deploy sw-cw-bq-gr-dash-gen \
+  --gen2 \
+  --runtime=python311 \
+  --region=us-west1 \
+  --source=src \
+  --entry-point=mk_gr_dsh \
+  --memory 16384MB \
+  --timeout 540s  \
+  --trigger-topic sw-cf-bq-pp-dt-rs \
+  --set-env-vars TEMPLATE_BUCKET=sw-eielson-dash-template
